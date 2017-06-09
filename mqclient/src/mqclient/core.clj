1109cdd438dc85762b5f9c7da73e2df49846461c
Laurence Chen <humorless@gmail.com>
2017-06-09 11:28:50 +0800

    let message queue client send json payload to message queue

--- mqclient/src/mqclient/core.clj
+++ mqclient/src/mqclient/core.clj
@@ -12,21 +12,24 @@
   (println (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s"
                    (String. payload "UTF-8") delivery-tag content-type type)))
 
-(def message 
-     {:endpoint "test-endpoint"
-      :metric "test-metric"
-      :timestamp 1488884708
-      :step 60
-      :value 10
-      :counterType "GAUGE"
-      :tags "idc=lg"})
+(def message
+  {:endpoint "test-endpoint"
+   :metric "test-metric"
+   :timestamp 1488884708
+   :step 60
+   :value 10
+   :counterType "GAUGE"
+   :tags "idc=lg"})
+
+(def payload
+  (json/write-str message))
 
 (defn -main
   [& args]
   (let [conn  (rmq/connect)
         ch    (lch/open conn)
         qname "queue.default"
-        bindkey "queue.default"
+        routingkey "queue.default"
         exchange "ex"]
     (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
     (lq/declare ch qname {:durable true :exclusive false :auto-delete false})
@@ -34,9 +37,9 @@
     ; when using default exchange, which name is "".
     ; we do not need to declare exchange or bind exchange with queue.
     ;
-    (lhe/declare ch exchange "direct") 
+    (lhe/declare ch exchange "direct")
     (lq/bind ch qname exchange)
-    (lb/publish ch exchange qname bindkey {:content-type "text/plain" :type "greetings.hi"})
+    (lb/publish ch exchange routingkey payload {:content-type "text/plain" :type "greetings.hi"})
     (Thread/sleep 2000)
     (println "[main] Disconnecting...")
     (rmq/close ch)
